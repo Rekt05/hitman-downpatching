f12d242b99b54e7f522ec5e51ce0db8e91cd6ac2
Rekt05 <coetzeereinhardt.rc@gmail.com>
2026-08-29 13:22:40 +0200

    create steam app id txt in the retail folder

--- assets/Hitman WoA Downpatch.bat
+++ assets/Hitman WoA Downpatch.bat
@@ -4,5 +4,6 @@ SET /P _manifest= Manifest ID:
 SET /P _username= Steam Username: 
 call DepotDownloader.exe -app 1847520 -depot 1659041 -manifest "%_manifest%" -user "%_username%" -remember-password -dir ".\HITMANWOADOWNPATCH" -validate
 rmdir ".\HITMANWOADOWNPATCH\.DepotDownloader" /s /q
-echo 1659040 >> .\HITMANWOADOWNPATCH\steam_appid.txt
+if not exist ".\HITMANWOADOWNPATCH\Retail" mkdir ".\HITMANWOADOWNPATCH\Retail"
+echo 1659040> ".\HITMANWOADOWNPATCH\Retail\steam_appid.txt"
 pause
